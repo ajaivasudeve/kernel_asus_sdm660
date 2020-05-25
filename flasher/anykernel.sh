4cc4fdd2f1b4278684d20ff3ca7fceb0277be992
ajaivasudeve <ajaivasudeve@gmail.com>
2020-05-25 21:34:23 +0530

    flasher: remove userspace patches
    Signed-off-by: ajaivasudeve <ajaivasudeve@gmail.com>

--- flasher/anykernel.sh
+++ flasher/anykernel.sh
@@ -32,36 +32,6 @@ ramdisk_compression=auto;
 ## AnyKernel install
 dump_boot;
 
-# begin userspace changes
-
-# mount vendor
-mount -o rw,remount -t auto /vendor >/dev/null;
-
-# backup files
-if [ ! -f /vendor/bin/init.qcom.post_boot.sh.bkp ]; then
-	cp -rpf /vendor/bin/init.qcom.post_boot.sh /vendor/bin/init.qcom.post_boot.sh.bkp;
-fi
-if [ ! -f /vendor/etc/thermal-engine.conf.bkp ]; then
-	cp -rpf /vendor/etc/thermal-engine.conf /vendor/etc/thermal-engine.conf.bkp;
-fi
-if [ ! -f /vendor/etc/perf/perfboostsconfig.xml.bkp ]; then
-	cp -rpf /vendor/etc/perf/perfboostsconfig.xml /vendor/etc/perf/perfboostsconfig.xml.bkp;
-fi
-
-# replace files
-cp -rpf $patch/init.qcom.post_boot.sh /vendor/bin/init.qcom.post_boot.sh;
-cp -rpf $patch/thermal-engine.conf /vendor/etc/thermal-engine.conf;
-cp -rpf $patch/perfboostsconfig.xml /vendor/etc/perf/perfboostsconfig.xml;
-cp -rpf $patch/perf-profile0.conf /vendor/etc/perf/perf-profile0.conf;
-
-# set up permissions
-chmod 0644 vendor/etc/perf/perf-profile0.conf;
-chmod 0777 /vendor/bin/init.qcom.post_boot.sh;
-chmod 0777 /vendor/etc/thermal-engine.conf;
-chmod 0644 /vendor/etc/perf/perfboostsconfig.xml;
-
-# end userspace changes
-
 write_boot;
 ## end install
 
